1553a47441c363d55ca2c82cc578d97bbb51f2ad
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-07-09 10:03:45 -0700

    further improvements to player framework

--- players.js
+++ players.js
@@ -2,31 +2,52 @@ var players = document.querySelectorAll('.player');
 
 // Takes a list of 4 names and populates the player section
 // empty string represents no-player
-var updatePlayers = function (plist) {
-    for (let i=0; i < plist.length; i++) {
-        if (plist[i] == ''){
+var updatePlayers = function (player_list,score_list) {
+    for (let i = 0; i < player_list.length; i++) {
+        if (player_list[i] == ''){
             //if name empty, then seat empty
             players[i].querySelector('.player-name').innerHTML = 'Empty';
-            players[i].classList.add('no-player')
-            console.log(players[i].classList)
+            players[i].querySelector('.player-score').innerHTML = '-';
+            players[i].classList.add('wait-player');
         } else {
             //if name exists, player exists
             //slice to only first 8 characters in name
-            players[i].querySelector('.player-name').innerHTML = plist[i].slice(0,8);
-            players[i].classList.remove('no-player')
-            console.log(players[i].classList)
+            players[i].querySelector('.player-name').innerHTML = player_list[i].slice(0,8);
+            players[i].querySelector('.player-score').innerHTML = score_list[i];
+            players[i].classList.remove('wait-player');
         };
     };
 };
+
+var startPlayers = function () {
+    for (let i = 0; i < players.length; i++) {
+        players[i].classList.add('started');
+    };
+}
+
+var finishPlayers = function () {
+    for (let i = 0; i < players.length; i++) {
+        players[i].classList.remove('started');
+    };
+}
 
 //Buttons to test visuals
 
 var add_players = document.querySelector('.add-players');
 var remove_players = document.querySelector('.remove-players');
+var start_game = document.querySelector('.start-game');
+var end_game = document.querySelector('.end-game');
+
 add_players.addEventListener('click', function () {
-    updatePlayers(['Derek','David','Dennis','Damien']);
+    updatePlayers(['Derek','David','Dennis',''],
+                  [12,14,16,18]);
 });
 
 remove_players.addEventListener('click', function () {
-    updatePlayers(['','','','']);
+    updatePlayers(['','','',''],
+                  [0,0,0,0]);
 });
+
+start_game.addEventListener('click', startPlayers);
+
+end_game.addEventListener('click', finishPlayers);
