1b9463fc27f0dd433c65dc5838e90bc758063ba4
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-07-08 20:20:33 -0700

    continued ui work

---
+++ modal.js
@@ -0,0 +1,28 @@
+var modal = document.querySelector('.modal');
+var closeButtons = document.querySelectorAll('.close-modal');
+var openButton = document.querySelector('.open-modal');
+var inner = document.querySelector('.modal-inner')
+var content = document.querySelector('.modal-content')
+
+// set open modal behaviour
+openButton.addEventListener('click', function() {
+    modal.classList.toggle('modal-open');
+    // document.body.scrollTop = document.documentElement.scrollTop = 0;
+});
+
+// set close modal behaviour
+for (i = 0; i < closeButtons.length; ++i) {
+closeButtons[i].addEventListener('click', function() {
+        modal.classList.toggle('modal-open');
+    });
+}
+
+// close modal if clicked outside content area
+inner.addEventListener('click', function() {
+    modal.classList.toggle('modal-open');
+});
+
+// prevent modal inner from closing parent when clicked
+content.addEventListener('click', function(e) {
+    e.stopPropagation();
+});
