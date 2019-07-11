0893e710bc5591189b5685857b4876d0fbddd66f
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-07-10 21:45:17 -0700

    improvements to ui styling and quiz framework

--- static/players.js
+++ static/players.js
@@ -1,58 +1,61 @@
-var players = document.querySelectorAll('.player');
+//global object for players framework
+var players = {};
+
+players.playerbars = document.querySelectorAll('.player');
 
 // Takes a list of 4 names and populates the player section
 // empty string represents no-player
-var updatePlayers = function (name_list,score_list) {
+players.updatePlayers = function (name_list,score_list) {
     for (let i = 0; i < name_list.length; i++) {
         if (name_list[i] == ''){
             //if name empty, then seat empty
-            players[i].querySelector('.player-name').innerHTML = 'Empty';
-            players[i].querySelector('.player-score').innerHTML = '-';
-            players[i].classList.add('wait-player');
+            players.playerbars[i].querySelector('.player-name').innerHTML = 'Empty';
+            players.playerbars[i].querySelector('.player-score').innerHTML = '-';
+            players.playerbars[i].classList.add('wait-player');
         } else {
             //if name exists, player exists
             //slice to only first 8 characters in name
-            players[i].querySelector('.player-name').innerHTML = name_list[i].slice(0,8);
-            players[i].querySelector('.player-score').innerHTML = score_list[i];
-            players[i].classList.remove('wait-player');
+            players.playerbars[i].querySelector('.player-name').innerHTML = name_list[i].slice(0,8);
+            players.playerbars[i].querySelector('.player-score').innerHTML = score_list[i];
+            players.playerbars[i].classList.remove('wait-player');
         };
     };
 };
 
 //starts game to hide missing player slots
-var startPlayers = function () {
-    for (let i = 0; i < players.length; i++) {
-        players[i].classList.add('started');
+players.startPlayers = function () {
+    for (let i = 0; i < players.playerbars.length; i++) {
+        players.playerbars[i].classList.add('started');
     };
-}
+};
 
 //ends game to reveal missing player slots
-var finishPlayers = function () {
-    for (let i = 0; i < players.length; i++) {
-        players[i].classList.remove('started');
+players.finishPlayers = function () {
+    for (let i = 0; i < players.playerbars.length; i++) {
+        players.playerbars[i].classList.remove('started');
     };
-}
+};
 
 //takes list of 4 booleans to display whether or not each player got the question right
-var judgePlayers = function (bool_list) {
-    for (let i = 0; i < players.length; i++) {
-        if (!players[i].classList.contains('wait-player')) {
+players.judgePlayers = function (bool_list) {
+    for (let i = 0; i < players.playerbars.length; i++) {
+        if (!players.playerbars[i].classList.contains('wait-player')) {
             if (bool_list[i]) {
-                players[i].classList.add('right');
+               players.playerbars[i].classList.add('right');
             } else {
-                players[i].classList.add('wrong');
+                players.playerbars[i].classList.add('wrong');
             };
         };
     };
-}
+};
 
 //unJudge all players to revert to default colors
-var unJudgePlayers = function () {
-    for (let i = 0; i < players.length; i++) {
-        players[i].classList.remove('right');
-        players[i].classList.remove('wrong');
+players.unJudgePlayers = function () {
+    for (let i = 0; i < players.playerbars.length; i++) {
+        players.playerbars[i].classList.remove('right');
+        players.playerbars[i].classList.remove('wrong');
     };
-}
+};
 
 //Buttons to test visuals
 
@@ -64,22 +67,21 @@ var judge_players = document.querySelector('.judge-players');
 var unjudge_players = document.querySelector('.unjudge-players');
 
 add_players.addEventListener('click', function () {
-    updatePlayers(['Derek','David','Dennis',''],
+    players.updatePlayers(['Derek','David','Dennis',''],
                   [12,14,16,18]);
 });
 
 remove_players.addEventListener('click', function () {
-    updatePlayers(['','','',''],
+    players.updatePlayers(['','','',''],
                   [0,0,0,0]);
 });
 
-start_game.addEventListener('click', startPlayers);
+start_game.addEventListener('click', players.startPlayers);
 
-end_game.addEventListener('click', finishPlayers);
+end_game.addEventListener('click', players.finishPlayers);
 
 judge_players.addEventListener('click', function () {
-    console.log('doing it');
-    judgePlayers([true,false,true,false]);
+    players.judgePlayers([true,false,true,false]);
 });
 
-unjudge_players.addEventListener('click', unJudgePlayers);
+unjudge_players.addEventListener('click', players.unJudgePlayers);
--- static/quiz.js
+++ static/quiz.js
@@ -1,2 +1,55 @@
-quiz_buttons = document.querySelectorAll('.quiz-button');
+var quiz = {}
 
+quiz.quiz_score = document.querySelector('.quiz-score');
+quiz.quiz_buttons = document.querySelectorAll('.quiz-button');
+
+quiz.hideQuiz = function () {
+    for (let i = 0; i <quiz. quiz_buttons.length; i++) {
+        quiz.quiz_buttons[i].classList.add('hidden');
+    };
+};
+
+quiz.showQuiz = function () {
+    for (let i = 0; i < quiz.quiz_buttons.length; i++) {
+        quiz.quiz_buttons[i].classList.remove('hidden');
+    };
+};
+
+//judges the options given as right or wrong after descision is made
+quiz.judgeQuiz = function (bool_list) {
+    for (let i = 0; i < quiz.quiz_buttons.length; i++) {
+        if (bool_list[i]) {
+            quiz.quiz_buttons[i].classList.add('right');
+        } else {
+            quiz.quiz_buttons[i].classList.add('wrong');
+        };
+    };
+}
+
+quiz.unJudgeQuiz = function () {
+    for (let i = 0; i < quiz.quiz_buttons.length; i++) {
+        quiz.quiz_buttons[i].classList.remove('right');
+        quiz.quiz_buttons[i].classList.remove('wrong');
+    };
+};
+
+// Button Choosing
+
+var choiceMade = false;
+
+//Buttons to test visuals
+
+var hide_quiz = document.querySelector('.hide-quiz');
+var show_quiz = document.querySelector('.show-quiz');
+var judge_quiz = document.querySelector('.judge-quiz');
+var unjudge_quiz = document.querySelector('.unjudge-quiz');
+
+hide_quiz.addEventListener('click', quiz.hideQuiz);
+
+show_quiz.addEventListener('click', quiz.showQuiz);
+
+judge_quiz.addEventListener('click', function () {
+    quiz.judgeQuiz([false,false,true,false]);
+});
+
+unjudge_quiz.addEventListener('click', quiz.unJudgeQuiz);
