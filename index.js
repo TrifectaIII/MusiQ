e15821c6293b46c40a9e91cfea1dd823c98ea36d
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-07-15 20:26:06 -0700

    added ability to send test messages in room, added room system

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
+// SETUP
+////////////////////////////////////////////////////////////////////
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 var io = require('socket.io')(serv);
-var default_port = 8000;
+var non_repl_port = 8000;
+
+// HTTP SERVER
+////////////////////////////////////////////////////////////////////
 
 //Start Server
-serv.listen(process.env.PORT || default_port);
+serv.listen(process.env.PORT || non_repl_port);
 console.log("Server started @ http://localhost:8000/");
 
 //route main page in index
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/static/index.html');
 });
-
 //Serve static files
 app.use('/static',express.static(__dirname + '/static'));
 
+// SOCKET HANDLING
+////////////////////////////////////////////////////////////////////
 
+//handle incoming socket connections
 io.sockets.on('connection', function (socket) {
+
+	//log a new connection
 	console.log('a new user connected. ID: ',socket.id);
+
+	//place socket in room
+    socket.on('get_room', function (room){
+		console.log(socket.id,'is joining',room);
+		
+		//if no room specified
+        if (room == 'NEW') {
+            let newRoom = Math.floor(Math.random() * 10000).toString();
+            socket.join(newRoom);
+			socket.emit('set_link', newRoom);
+			
+		//if room specified
+        } else {
+            socket.join(room);
+            socket.emit('set_link', +room);
+        }
+        
+	});
+	
+	// for test messages between clients in room
+	socket.on('test-message', function () {
+		for (let room in socket.rooms){
+			io.sockets.in(room).emit('test-message-recieved',socket.id);
+		}
+	});
 });
--- static/main.js
+++ static/main.js
@@ -1,1 +1,35 @@
+//setup socket io
 var socket = io();
+
+//Look for Room
+var room;
+var url = window.location.href;
+
+//If no room in url, tell server to create new one
+var roomIndex = url.search('room=');
+if (roomIndex == -1){
+    room = 'NEW';
+} else {
+    room = url.slice(roomIndex+5);
+};
+
+//on connection, ask for room
+socket.on('connect',function () {
+    socket.emit('get_room',room);
+});
+
+//set multiplayer link once room is created/joined
+socket.on('set_link', function (room_name) {
+    link.setLink(url.slice(0,url.lastIndexOf('/')+1)+'?room='+room_name);
+    //also change url of clients browser to match room link
+    history.pushState({},"",url.slice(0,url.lastIndexOf('/')+1)+'?room='+room_name);
+});
+
+
+// code for sending/recieving test messages to/from server
+document.querySelector('.test-message').addEventListener('click', function() {
+    socket.emit('test-message');
+});
+socket.on('test-message-recieved', function (id) {
+    console.log('test-message-recieved',id);
+});
